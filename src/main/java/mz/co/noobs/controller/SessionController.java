d3c9d17356b9a1a0d7aff3cbbbc6f0a7cb067209
Ted Nicolas António Fernandes <ted.fernandess@gmail.com>
2017-03-06 13:17:22 +0200

    Mandando a mensagem de sucesso ou nao sucesso para a tela

--- src/main/java/mz/co/noobs/controller/SessionController.java
+++ src/main/java/mz/co/noobs/controller/SessionController.java
@@ -1,6 +1,7 @@
 package mz.co.noobs.controller;
 
 import org.springframework.stereotype.Controller;
+import org.springframework.ui.Model;
 import org.springframework.web.bind.annotation.RequestMapping;
 
 import mz.co.noobs.exceptions.SSHConnectionException;
@@ -17,21 +18,23 @@ public class SessionController {
 	}
 
 	@RequestMapping("/conectarServidor")
-	public String ConectarServidor(Acesso acesso) {
+	public String ConectarServidor(Acesso acesso, Model model) {
 
 		ConnectService connectService;
+		String resultado;
 
 		try {
 			connectService = new ConnectService(acesso);
 			connectService.init();
 			int execute = connectService.execute("ls");
-			String resultado = LoadProperties.getValue(String.valueOf(execute));
-			System.out.println(resultado);
+			resultado = LoadProperties.getValue(String.valueOf(execute));
+			model.addAttribute("resultado", resultado);
 			connectService.closeSession();
+			return "conectado";
 		} catch (SSHConnectionException e) {
-			System.out.println(e.getMessage() + " "+e.getCause());
+			resultado = e.getMessage();
+			model.addAttribute("resultado", resultado);
+			return "naoConectado";
 		}
-		return "conectado";
 	}
-	
 }
